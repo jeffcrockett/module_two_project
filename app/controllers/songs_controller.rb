510604a596a7ccdca9f771169f4c4967e3d1ade1
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2018-08-17 20:22:26 -0400

    Added bootstrap

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,13 @@
 class SongsController < ApplicationController
+    before_action :set_song, only: [:show, :edit, :update, :destroy]
+    def index
+        @songs = Song.all
+    end
+
+    private
+
+    def set_song
+        @song = Song.find(params[:id])
+    end
+
 end
