ab7b362177a292a0febd8774c89e0b4d5d08d716
aubreeabril <aubree.abril@gmail.com>
2018-08-21 17:27:11 -0400

    formatted search results page

--- db/test_api.rb
+++ db/test_api.rb
@@ -14,8 +14,8 @@ class Parser
     end
     def album_ids
         json = JSON.parse(RestClient.get("https://api.musixmatch.com/ws/1.1/artist.albums.get?format=jsonp&callback=callback&artist_id=#{@artist_id}&page_size=100&apikey=1d628b26f4bb8472f6ad706b87f6be07").body[9..-3])
-        json['message']['body']['album_list'].each_with_object({}) do |album, obj| 
-            name = album['album']['album_name'] 
+        json['message']['body']['album_list'].each_with_object({}) do |album, obj|
+            name = album['album']['album_name']
             id = album['album']['album_id']
             obj[name] = id
         end
@@ -27,7 +27,7 @@ class Parser
         json['message']['body']['track_list'].each_with_object({}) do |track, obj|
             name = track['track']['track_name']
             id = track['track']['track_id']
-            obj[name] = id 
+            obj[name] = id
         end
     # binding.pry
     end
@@ -35,7 +35,7 @@ class Parser
     def lyrics_from(track_id)
         json = JSON.parse(RestClient.get("https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&track_id=#{track_id}&apikey=1d628b26f4bb8472f6ad706b87f6be07").body[9..-3])
         # binding.pry
-        #   binding.pry 
+        #   binding.pry
         if json['message']['header']['status_code'] == 404
             return ['No lyrics']
         else
@@ -46,7 +46,7 @@ class Parser
     def albums_songs_hash(ids)
         #ids is output of album_ids
         ids.each_with_object({}) do |(key, value), obj| obj[key] = album_tracks_from(value) end
-       
+
     end
 
     def albums_songs_lyrics_hash(albums_songs)
@@ -54,29 +54,29 @@ class Parser
         # binding.pry
         albums_songs.each_with_object({}) do |(album_name, song_name_hash), obj|
             #IT BREAKS HERE
-            
+
             song_name_hash.each do |song_name, song_id|
-              
+
                 obj[album_name] ||= []
-                obj[album_name] << {song_name => lyrics_from(song_id)} 
-       
-            end 
-                      
+                obj[album_name] << {song_name => lyrics_from(song_id)}
+
+            end
+
         end
-       
+
     end
 
     def seed(artist_name)
         new_artist = Artist.find_or_create_by(name: artist_name)
-      
+
         seed_hash = albums_songs_lyrics_hash(albums_songs_hash(album_ids))
         #BROKEN HERE
         # binding.pry
         seed_hash.each do |album_name, song_hash_array|
-    
+
         album = Album.find_or_create_by(name: album_name, artist: new_artist)
         song_hash_array.each do |hash|
-            hash.each do |song_name, lyrics_array|       
+            hash.each do |song_name, lyrics_array|
                 song = Song.find_or_create_by(name: song_name, album: album)
                 lyrics_array[0..-5].each do |line|
                 Line.find_or_create_by(name: line, song: song)
